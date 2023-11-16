dbae56fbe1a6ef8425ac1e2c894933b0813f0f8f
Ricardo Sanchez <ricsanfre@gmail.com>
2023-11-16 22:26:06 +0100

    Adding Microservices Discovery Service: Eureka Server

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 - [Microservices with Spring Cloud](https://spring.io/microservices)
 - [Spring Cloud](https://spring.io/cloud)
+- [Microservices Discovery Service: Eureka ](https://cloud.spring.io/spring-cloud-netflix/reference/html/)
--- customer/src/main/java/com/ricsanfre/microservices/customer/CustomerService.java
+++ customer/src/main/java/com/ricsanfre/microservices/customer/CustomerService.java
@@ -32,7 +32,7 @@ public class CustomerService {
         customerRepository.saveAndFlush(customer);
         // Check if fraudster using fraud microservice
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
